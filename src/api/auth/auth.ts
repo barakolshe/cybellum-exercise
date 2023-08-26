9be7071ba091f4df707dd2d101eb5f4648fbe83b
barakolshe <barakolshe@gmail.com>
2023-08-26 12:29:54 +0300

    added .env.sample file

--- src/api/auth/auth.ts
+++ src/api/auth/auth.ts
@@ -1,16 +1,16 @@
-import { createApi } from '@reduxjs/toolkit/query/react';
-import baseQuery from 'helpers/baseQuery';
+import { createApi } from "@reduxjs/toolkit/query/react";
+import baseQuery from "helpers/baseQuery";
 
-import { AuthRequestType, AuthResponseType } from './auth.interface';
+import { AuthRequestType, AuthResponseType } from "./auth.interface";
 
 export const authApi = createApi({
-  reducerPath: 'authApi',
-  baseQuery: baseQuery('/'),
+  reducerPath: "authApi",
+  baseQuery: baseQuery("/"),
   endpoints: (builder) => ({
     login: builder.mutation<AuthResponseType, AuthRequestType>({
       query: (body) => ({
-        url: '/login',
-        method: 'POST',
+        url: "/login",
+        method: "POST",
         body,
       }),
     }),
